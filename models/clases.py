c37bb86d67b8b8927a4ea0c52e6013953c6b57c8
OctavioNovelo <octavionovelo22@gmail.com>
2024-06-10 03:52:50 +0000

    Places

--- models/clases.py
+++ models/clases.py
@@ -15,3 +15,21 @@ class User(BaseModel):
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
+
+class Place(BaseModel):
+    def __init__(self, name, description, address, city, latitude, longitude, host_id, num_rooms, num_bathrooms, price_per_night, max_guests):
+        super().__init__()
+        self.name = name
+        self.description = description
+        self.address = address
+        self.city = city
+        self.latitude = latitude
+        self.longitude = longitude
+        self.host_id = host_id
+        self.num_rooms = num_rooms
+        self.num_bathrooms = num_bathrooms
+        self.price_per_night = price_per_night
+        self.max_guests = max_guests
+        self.amenities = []
+        self.reviews = []
+
